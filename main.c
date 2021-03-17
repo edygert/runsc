b688e7afb0959675a9127afb7c13e5ccd684e50a
Evan H. Dygert <evand@dygertconsulting.com>
2021-03-17 18:03:13 -0400

    Completely rewrote the command line processing.

--- main.c
+++ main.c
@@ -12,6 +12,8 @@
 #include "Shlobj.h"
 #include "shlwapi.h"
 #include "windows.h"
+#include "getopt.h"
+
 #pragma comment(lib, "Shlwapi.lib")
 #pragma comment(lib, "Ws2_32.lib")
 
@@ -19,61 +21,125 @@ const DWORD MEGABYTE = 1024 * 1024;
 
 const char* shellcodefilename = "\\shellcode";
 
-const char* helpText = "\nUsage: runsc <offset> or runsc <offset> <filename of document to open for shellcode to find>\n\n"
-	"Shellcode must be in the current directory in a file named 'shellcode'.\n\n"
-	"The offset is required and must be the first parameter, but will often be 0.\n"
-	"The offset may be entered using decimal numbers or hex (prefixed by 0x).\n\n"
-	"Some shellcode looks for the next malware stage in the document in which\n"
-	"it is embedded. Supply the filename of that document as the third parameter\n"
-	"for that malware. This parameter is optional.\n\n"
-	"Run this program from the command line, after which you will need to\n"
-	"attach to it using the debugger. The address of the shellcode will be\n"
-	"printed on the screen. This is the address at which to set a breakpoint\n"
-	"in the debugger."
-	"\n\n";
+const char* helpText =
+"\nUsage: runsc -f <shellcode file> -o <offset> -d <document file> -n\n\n"
+"\n-h: Display this help text.\n"
+"-f: The name of the file in which shellcode resides (required).\n"
+"-o: The offset at which the shellcode begins (optional, default is 0). The offset may\n"
+"be entered using decimal numbers or hex (prefixed by 0x).\n"
+"-d: The name of a document file that shellcode may need to be loaded in memory (optional).\n"
+"Some shellcode looks for the next malware stage in the document in which\n"
+"it is embedded. Supply the filename of that document for this parameter.\n"
+"-n: The default behavior is to run the shellcode as a suspended thread to give\n"
+"you time to attach to this process with a debugger. If you just want to run the\n"
+"shellcode and monitor it using behavior analysis tool, specify this option.\n\n"
+"Run this program from the command line, after which you will need to\n"
+"attach to it using the debugger (if -n is not specified). The address of the shellcode\n"
+"will be printed on the screen. This is the address at which to set a breakpoint\n"
+"in the debugger."
+"\n\n";
+
+// This code is from https://stackoverflow.com/questions/8046097/how-to-check-if-a-process-has-the-administrative-rights
+static BOOL IsElevated()
+{
+	BOOL fRet = FALSE;
+	HANDLE hToken = NULL;
+	if (OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken)) {
+		TOKEN_ELEVATION Elevation;
+		DWORD cbSize = sizeof(TOKEN_ELEVATION);
+		if (GetTokenInformation(hToken, TokenElevation, &Elevation, sizeof(Elevation), &cbSize)) {
+			fRet = Elevation.TokenIsElevated;
+		}
+	}
+	if (hToken) {
+		CloseHandle(hToken);
+	}
+	return fRet;
+}
 
 int main(int argc, char** argv)
 {
-	if (argc < 2 || (argc >= 2 && strcmp(argv[1], "-h") == 0)) {
-		printf(helpText);
+	char* shellcodeFile = NULL;
+	int radix = 10;
+	DWORD offset = 0;
+	char* docFile = NULL;
+	BOOL nopause = FALSE;
+
+	int c;
+	while ((c = getopt(argc, argv, "f:o:d:nh")) != -1) {
+		switch (c) {
+		case 'h':
+			printf(helpText);
+			return 1;
+
+		case 'f':
+			shellcodeFile = optarg;
+			break;
+
+		case 'o':
+			if (strchr(optarg, 'x') || strchr(optarg, 'X')) {
+				radix = 16;
+			}
+
+			offset = (DWORD)strtol(optarg, NULL, radix);
+			if (offset < 0) {
+				printf("[!] Error: Offset must be between 0 and the size of the shellcode.");
+				return 1;
+			}
+
+			// if strtol returned 0 but the user specified something other than 0, 0x0, or 0X0, then
+			// the offset was invalid.
+			if (offset == 0 && (strcmp(optarg, "0") != 0 || _stricmp(optarg, "0x") != 0)) {
+				printf("[!] Error: Offset must be between 0 and the size of the shellcode.");
+				return 1;
+			}
+			break;
+
+		case 'd':
+			docFile = optarg;
+			break;
+
+		case 'n':
+			nopause = TRUE;
+			break;
+
+		case '?':
+			printf(helpText);
+			return 1;
+
+		default:
+			return 1;
+		}
+	}
+
+	if (shellcodeFile == NULL) {
+		printf("[!] Error: shellcode file must be specified.");
 		return 1;
 	}
 
-	// This was only included so that the ws2_32.dll
-	// library would be loaded; easier to set breakpoints!
+	if (!PathFileExistsA(shellcodeFile)) {
+		printf("[!] Error: Shellcode file not found: %s\n\n", shellcodeFile);
+		return 1;
+	}
+	printf("[*] Shellcode file: %s\n\n", shellcodeFile);
+
+	if (docFile != NULL) {
+		if (!PathFileExistsA(docFile)) {
+			printf("[!] Error: Document file not found: %s\n\n", docFile);
+			return 1;
+		}
+		printf("[*] Document file: %s\n\n", docFile);
+	}
+
+	// This was only included so that ws2_32.dll would be loaded; easier to set breakpoints!
 	WSADATA wsaData;
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	if (IsUserAnAdmin()) {
+	if (!IsElevated()) {
 		printf("[!] Warning: You may not be running with admin rights. It is recommended that you do so when analyzing shellcode :)\n\n");
 	}
 
-	// The full path to the shellcode file
-	char fullPath[1024];
-
-	memset(fullPath, 0, sizeof(fullPath));
-
-	DWORD dwResult = GetCurrentDirectoryA(sizeof(fullPath), fullPath);
-	if (dwResult == 0) {
-		printf("[!] Error: GetCurrentDirectoryA failed\n");
-		return 1;
-	}
-	printf("[*] The current \"working\" directory is: %s\n\n", fullPath);
-
-	if (strlen(fullPath) + strlen(shellcodefilename) >= sizeof(fullPath)) {
-		printf("[!] Error: length of full path to shellcode file is >= %d\n", (unsigned int)sizeof(fullPath));
-		return 1;
-	}
-
-	strcat_s(fullPath, sizeof(fullPath), shellcodefilename);
-
-	if (!PathFileExistsA(fullPath)) {
-		printf("[!] Error: File not found: %s\n\n", fullPath);
-		return 1;
-	}
-	printf("[*] File found: %s\n\n", fullPath);
-
-	HANDLE hSCFile = CreateFileA(fullPath, GENERIC_READ,
+	HANDLE hSCFile = CreateFileA(shellcodeFile, GENERIC_READ,
 		FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL,
 		OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
 
@@ -86,20 +152,7 @@ int main(int argc, char** argv)
 	}
 	printf("[*] Size of shellcode is %u bytes\n\n", fsLow);
 
-	int radix = 10;
-	if (strchr(argv[1], 'x') || strchr(argv[1], 'X')) {
-		radix = 16;
-	}
-
-	DWORD offset = (DWORD)strtol(argv[1], NULL, radix);
-	if (offset >= fsLow || offset < 0) {
-		printf("[!] Error: Offset must be between 0 and the size of the shellcode.");
-		return 1;
-	}
-
-	// if strtol returned 0 but the user specified something other than 0, 0x0, 0X0, then
-	// the offset was invalid.
-	if (offset == 0 && (strcmp(argv[1], "0") != 0 || _stricmp(argv[1], "0x") != 0)) {
+	if (offset >= fsLow) {
 		printf("[!] Error: Offset must be between 0 and the size of the shellcode.");
 		return 1;
 	}
@@ -122,20 +175,27 @@ int main(int argc, char** argv)
 
 	printf("[*] Successfully read %u bytes into buffer!\n\n", fsLow);
 
-	if (argc == 3) {
-		HANDLE hDoc = CreateFileA(argv[2], GENERIC_READ, FILE_SHARE_READ, NULL,
+	if (docFile != NULL) {
+		HANDLE hDoc = CreateFileA(docFile, GENERIC_READ, FILE_SHARE_READ, NULL,
 		   OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 		if (hDoc == INVALID_HANDLE_VALUE) {
-			printf("[!] Error: Could not open document: %s\n\n", argv[2]);
+			printf("[!] Error: Could not open document file: %s\n\n", docFile);
 			return 1;
 		}
-		printf("[*] Doc file handle: %p\n\n", hDoc);
+		printf("[*] Document file handle: %p\n\n", hDoc);
 	}
 
-	// Create a suspended thread on the shellcode, adding the shellcode's location
-	// as an argument (in case it's needed)
+	// Create the thread in a suspended or not state, depending on the -n option.
+	// Also pass the address of the shellcode as a parameter because some
+	// shellcode expects its address to be on the stack.
 	DWORD threadID = 0;
-	HANDLE hThread = CreateThread(NULL, MEGABYTE, (LPTHREAD_START_ROUTINE)(scBuffer + offset), scBuffer + offset, CREATE_SUSPENDED, &threadID);
+	char *shellcodeAddress = scBuffer + offset;
+	DWORD creationFlags = CREATE_SUSPENDED;
+	if (nopause) {
+		creationFlags = 0;
+	}
+
+	HANDLE hThread = CreateThread(NULL, MEGABYTE, (LPTHREAD_START_ROUTINE)(scBuffer + offset), &shellcodeAddress, creationFlags, &threadID);
 	if (hThread == NULL) {
 		printf("[!] Could not create shellcode thread.\n");
 		return 1;
@@ -148,3 +208,4 @@ int main(int argc, char** argv)
 
 	return 0;
 }
+
